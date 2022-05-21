42b43d79af068fe16b67342ae5e0b7fbced0c2d3
Marcel Witoschek <marcel@marciland.de>
2022-05-21 18:40:27 +0200

    new repo structure adjustment

--- src/main/java/de/marciland/utilities/Constants.java
+++ src/main/java/de/marciland/utilities/Constants.java
@@ -1,7 +1,7 @@
 package de.marciland.utilities;
 
 public class Constants {
-    public static final String resourcePath = "futter/src/main/resources/";
+    public static final String resourcePath = "src/main/resources/";
     public static final String imagePath = resourcePath + "images/";
     public static final String profilePath = resourcePath + "profiles/";
     public static final String recipePath = resourcePath + "recipes/";
