2705d8269d1c46934ffb5bc3e027aff974c0bbed
Marcel Witoschek <marcel@marciland.de>
2022-05-21 18:48:46 +0200

    add type to ingredient

--- src/main/java/de/marciland/recipehandler/Ingredient.java
+++ src/main/java/de/marciland/recipehandler/Ingredient.java
@@ -5,6 +5,14 @@ public class Ingredient {
 
     private String name = "";
     /*
+     * type:
+     * 0 = undefined
+     * 1 = solid, should be in gramms
+     * 2 = liquid, should be in milliliters
+     * 3 = countable, should be in amount by pieces
+     */
+    private int type = 0;
+    /*
      * nutrition information for 100g of ingredient.
      */
     private float kcal = 0;
@@ -12,14 +20,19 @@ public class Ingredient {
     private float carbs = 0;
     private float protein = 0;
 
-    public Ingredient(String name, float kcal, float fat, float carbs, float protein) {
+    public Ingredient(String name, int type, float kcal, float fat, float carbs, float protein) {
         this.name = name;
+        if (type != 1 && type != 2 && type != 3) {
+            // TODO handle wrong type
+        }
+        this.type = type;
         this.kcal = kcal;
         this.fat = fat;
         this.carbs = carbs;
         this.protein = protein;
     }
 
+    // TODO change toString for ing
     @Override
     public String toString() {
         return getName() + " hat auf 100g:"
@@ -34,6 +47,10 @@ public class Ingredient {
      */
     public String getName() {
         return this.name;
+    }
+
+    public int getType() {
+        return type;
     }
 
     public float getKcal() {
