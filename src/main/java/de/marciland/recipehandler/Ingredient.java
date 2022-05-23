ea23cb045de6d9ef0fa0a557afda236dfb2e39f0
Marcel Witoschek <92092148+Marciland@users.noreply.github.com>
2022-05-23 15:58:25 +0200

    Ingredient extension (#6)
    * adjust for ingredient type
    
    * adjust for ingredient type
    
    * new repo structure adjustment
    
    * constant path added to loadAllIngredients
    
    * better readability for loadingredient errors
    
    * add type to ingredient
    
    * todo done, adjusted documentation
    
    * fixed toString for ingredient and added type to test.ing
    
    * error handling for wrong type
    
    * test build
    
    * build test
    
    * ...

--- src/main/java/de/marciland/recipehandler/Ingredient.java
+++ src/main/java/de/marciland/recipehandler/Ingredient.java
@@ -1,19 +1,31 @@
 package de.marciland.recipehandler;
 
-//TODO ingredient either is liquid/solid/or countable
 public class Ingredient {
 
     private String name = "";
     /*
-     * nutrition information for 100g of ingredient.
+     * type:
+     * 0 = undefined
+     * 1 = solid, should be in gramms
+     * 2 = liquid, should be in milliliters
+     * 3 = countable, should be in amount by pieces
+     */
+    private int type = 0;
+    /*
+     * nutrition information for 100g/100ml/1piece of ingredient.
      */
     private float kcal = 0;
     private float fat = 0;
     private float carbs = 0;
     private float protein = 0;
 
-    public Ingredient(String name, float kcal, float fat, float carbs, float protein) {
+    public Ingredient(String name, int type, float kcal, float fat, float carbs, float protein) {
         this.name = name;
+        if (type != 1 && type != 2 && type != 3) {
+            System.out.println("Ingredient: " + getName() + " has an invalid type!");
+            System.exit(1);
+        }
+        this.type = type;
         this.kcal = kcal;
         this.fat = fat;
         this.carbs = carbs;
@@ -22,7 +34,23 @@ public class Ingredient {
 
     @Override
     public String toString() {
-        return getName() + " hat auf 100g:"
+        String typeStr = null;
+        switch (this.type) {
+            case 1:
+                typeStr = " hat auf 100g:";
+                break;
+            case 2:
+                typeStr = " hat auf 100ml:";
+                break;
+            case 3:
+                typeStr = " hat pro Stück:";
+                break;
+        }
+        if (typeStr == null) {
+            System.out.println("Ingredient: " + getName() + " has an invalid type!");
+            System.exit(1);
+        }
+        return getName() + typeStr
                 + " Kcal: " + getKcal()
                 + " Fett: " + getFat()
                 + " Zucker: " + getCarbs()
@@ -34,6 +62,10 @@ public class Ingredient {
      */
     public String getName() {
         return this.name;
+    }
+
+    public int getType() {
+        return type;
     }
 
     public float getKcal() {
--- src/main/java/de/marciland/recipehandler/Loader.java
+++ src/main/java/de/marciland/recipehandler/Loader.java
@@ -206,6 +206,7 @@ public class Loader {
                             /*
                              * wait until user input is valid.
                              */
+                            // TODO check conditions
                             while (input.length() < 2 || input.length() > 4) {
                                 input = JOptionPane.showInputDialog(parent.getContentPane(), "Wie viel wiegst du?",
                                         "Bitte Gewicht eingeben!", JOptionPane.QUESTION_MESSAGE);
@@ -358,7 +359,7 @@ public class Loader {
          * check all files in ingredient directory.
          * Name of file is added to ingredientList if it's a file and ends with ".ing".
          */
-        File ingredientFolder = new File("futter/src/main/resources/ingredients");
+        File ingredientFolder = new File(ingredientPath);
         File[] allIngredientsFiles = ingredientFolder.listFiles();
         List<String> ingredientList = new ArrayList<>();
         for (File file : allIngredientsFiles) {
@@ -384,6 +385,7 @@ public class Loader {
 
     private static Ingredient loadIngredient(String file) {
         String name = null;
+        int type = 0;
         float kcal = 0;
         float fat = 0;
         float carbs = 0;
@@ -396,6 +398,10 @@ public class Loader {
                 if (line.startsWith("Name")) {
                     line = line.replace("Name = ", "");
                     name = line;
+                }
+                if (line.startsWith("Typ")) {
+                    line = line.replace("Typ = ", "");
+                    type = Integer.parseInt(line);
                 }
                 if (line.startsWith("kcal")) {
                     line = line.replace("kcal = ", "");
@@ -417,12 +423,14 @@ public class Loader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (name == null || kcal == 0) {
-            System.out.println("name or kcal empty!");
+        if (name == null || type == 0 || kcal == 0) {
+            System.out.println(System.lineSeparator() + "Error when loading " + file + " ingredient:");
+            System.out.println("name, type or kcal empty!" + System.lineSeparator());
         }
         if (fat == 0 && carbs == 0 && protein == 0) {
-            System.out.println("wrong information about nutrition!");
+            System.out.println(System.lineSeparator() + "Error when loading " + file + " ingredient:");
+            System.out.println("wrong information about nutrition!" + System.lineSeparator());
         }
-        return new Ingredient(name, kcal, fat, carbs, protein);
+        return new Ingredient(name, type, kcal, fat, carbs, protein);
     }
 }
