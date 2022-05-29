8e036bfe9403cfd68e132d2c06e88552810f9279
Marcel Witoschek <marcel@marciland.de>
2022-05-30 01:32:14 +0200

    layout for get ingredient information

--- src/main/java/de/marciland/windowhandler/Dialog.java
+++ src/main/java/de/marciland/windowhandler/Dialog.java
@@ -4,8 +4,28 @@ import de.marciland.ingredienthandler.Ingredient;
 
 public class Dialog {
 
+    // TODO doc
     public static Ingredient getIngredientInformation() {
-        return null;
+        String name = null;
+        int type = 0;
+        float kcal = 0;
+        float fat = 0;
+        float carbs = 0;
+        float protein = 0;
+        // TODO ask for input
+        if (name == null) {
+            System.out.println("Name empty while getting ingredient information");
+            System.exit(1);
+        }
+        if (type != 1 || type != 2 || type != 3) {
+            System.out.println("Wrong type while getting ingredient information");
+            System.exit(1);
+        }
+        if (fat == 0 && carbs == 0 && protein == 0) {
+            System.out.println("Nutrition information empty while getting ingredient information!");
+            System.exit(1);
+        }
+        return new Ingredient(name, type, kcal, fat, carbs, protein);
     }
 
 }
