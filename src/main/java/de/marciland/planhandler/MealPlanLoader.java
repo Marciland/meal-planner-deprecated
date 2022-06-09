12443b7a1921daabaf0a4f0665d93de487658e66
Marcel Witoschek <marcel@marciland.de>
2022-06-09 19:34:00 +0200

    reading done, next set values

--- src/main/java/de/marciland/planhandler/MealPlanLoader.java
+++ src/main/java/de/marciland/planhandler/MealPlanLoader.java
@@ -1,12 +1,22 @@
 package de.marciland.planhandler;
 
 import static de.marciland.utilities.Constants.planColumns;
+import static de.marciland.utilities.Constants.planFile;
 import static de.marciland.utilities.Constants.planRows;
 
 import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
+
+import de.marciland.recipehandler.Recipe;
+import de.marciland.recipehandler.RecipeLoader;
+import de.marciland.utilities.Tools;
 
 /**
  * Loader class for the meal plan. Used to load from and save to files.
@@ -25,7 +35,57 @@ public class MealPlanLoader {
     public static JLabel[][] loadMealPlan() {
         // if no file exists return null
         JLabel[][] labels = prepareEmptyPlan();
-        // TODO read meal plan from file
+        Recipe[] recipes = new Recipe[7];
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(new File(planFile)));
+            String line;
+            reading: while (true) {
+                line = reader.readLine();
+                if (line == null) {
+                    break reading;
+                }
+                if (line.startsWith("Montag")) {
+                    line = line.replace("Montag = ", "").trim();
+                    recipes[0] = RecipeLoader.loadRecipe(line + ".rec");
+                }
+                if (line.startsWith("Dienstag")) {
+                    line = line.replace("Dienstag = ", "").trim();
+                    recipes[1] = RecipeLoader.loadRecipe(line + ".rec");
+                }
+                if (line.startsWith("Mittwoch")) {
+                    line = line.replace("Mittwoch = ", "").trim();
+                    recipes[2] = RecipeLoader.loadRecipe(line + ".rec");
+                }
+                if (line.startsWith("Donnerstag")) {
+                    line = line.replace("Donnerstag = ", "").trim();
+                    recipes[3] = RecipeLoader.loadRecipe(line + ".rec");
+                }
+                if (line.startsWith("Freitag")) {
+                    line = line.replace("Freitag = ", "").trim();
+                    recipes[4] = RecipeLoader.loadRecipe(line + ".rec");
+                }
+                if (line.startsWith("Samstag")) {
+                    line = line.replace("Samstag = ", "").trim();
+                    recipes[5] = RecipeLoader.loadRecipe(line + ".rec");
+                }
+                if (line.startsWith("Sonntag")) {
+                    line = line.replace("Sonntag = ", "").trim();
+                    recipes[6] = RecipeLoader.loadRecipe(line + ".rec");
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException f) {
+            // should not happen due to checks earlier in this method.
+        } catch (IOException e) {
+            System.out.println("Error while reading " + planFile);
+            System.exit(1);
+        }
+        if (Tools.arrayContainsNull(recipes)) {
+            System.out.println("Reading meal plan from file failed! Not all recipes have been saved!");
+            System.exit(1);
+        }
+        // TODO set values from recipes into meal plan labels
         return labels;
     }
 
