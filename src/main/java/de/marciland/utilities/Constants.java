6bd26e37c20d311042b895917d7c97273bfffd71
Marcel Witoschek <marcel@marciland.de>
2022-06-09 12:47:10 +0200

    more documentations

--- src/main/java/de/marciland/utilities/Constants.java
+++ src/main/java/de/marciland/utilities/Constants.java
@@ -1,12 +1,39 @@
 package de.marciland.utilities;
 
+/**
+ * Constants that are regularly used.
+ */
 public class Constants {
+    /**
+     * location of all resource files.
+     */
     public static final String resourcePath = "src/main/resources/";
+    /**
+     * location of all images used as resource.
+     */
     public static final String imagePath = resourcePath + "images/";
+    /**
+     * location of profile information.
+     */
     public static final String profilePath = resourcePath + "profiles/";
+    /**
+     * location where recipes are saved.
+     */
     public static final String recipePath = resourcePath + "recipes/";
+    /**
+     * location where ingredients are saved.
+     */
     public static final String ingredientPath = resourcePath + "ingredients/";
+    /**
+     * helper folder containing usable files.
+     */
     public static final String helperPath = resourcePath + "helper/";
+    /**
+     * amount of columns for the meal plan.
+     */
     public static final int planColumns = 8;
+    /**
+     * amount of rows for the meal plan.
+     */
     public static final int planRows = 7;
 }
